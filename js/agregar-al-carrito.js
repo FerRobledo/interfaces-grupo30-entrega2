ce50ee617abc91644fa95e91493eff0ff1b88d04
Fermin Robledo <fermin4robledo@gmail.com>
2024-10-14 10:41:59 -0300

    Ultimos arreglos responsive, implementacion boton compartir, cambios en imagenes

--- js/agregar-al-carrito.js
+++ js/agregar-al-carrito.js
@@ -1,19 +1,22 @@
-function mostrarOverlay(div) {
-    const tarjeta = div.closest('.tarjeta-juego');
-    const overlay = tarjeta.querySelector('.overlay-carrito');
-    const imagenBoton = div.querySelector('img');
+
+btnCarrito = document.querySelectorAll('.btn-carrito');
+
+btnCarrito.forEach(btnCarro => {
+    btnCarro.addEventListener('click', function () {
+        let tarjeta = btnCarro.closest('.tarjeta-juego');
+        let overlay = tarjeta.querySelector('.overlay-carrito');
+        console.log(overlay);
+        let imagenBoton = btnCarro.querySelector('img');
 
 
-    overlay.classList.toggle('show');
+        overlay.classList.toggle('show');
 
-    if (overlay.classList.contains('show')) {
-        imagenBoton.src = './images/Botones-tarjeta-1.png'; 
-    } else {
-        imagenBoton.src = './images/Botones-tarjeta.png';
-    }
-}
-
-
-document.querySelectorAll('.btn-carrito').forEach(div => {
-    div.addEventListener('click', () => mostrarOverlay(div));
+        if (overlay.classList.contains('show')) {
+            imagenBoton.src = './images/Botones-tarjeta-1.png';
+        } else {
+            imagenBoton.src = './images/Botones-tarjeta.png';
+        }
+    })
 });
+
+
